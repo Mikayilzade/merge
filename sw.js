0dded25d08b814166eb298d6911d320c7c3591b4
Mikayilzade <m.mikayil.z@gmail.com>
2026-07-25 23:12:56 +0400

    Cache 0.3.6 watchdog build

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'merge-arcana-v8-0.3.5';
+const CACHE = 'merge-arcana-v9-0.3.6';
 const CORE = [
-  './','./index.html','./wiki.html?v=0.3.5','./styles.css?v=0.3.5','./styles-v03.css?v=0.3.5',
-  './manifest.webmanifest?v=0.3.5','./assets/icon.svg?v=0.3.5','./js/app.js?v=0.3.5','./js/data.js?v=0.3.5',
-  './js/engine-v03.js?v=0.3.5','./js/engine-v031.js?v=0.3.5','./js/engine-v033.js?v=0.3.5','./js/characters.js?v=0.3.5',
-  './js/v032-ui.js?v=0.3.5','./js/v032-formation.js?v=0.3.5','./js/v032-combat-ui.js?v=0.3.5','./js/v033-state.js?v=0.3.5','./js/v034-ui.js?v=0.3.5'
+  './','./index.html','./wiki.html?v=0.3.6','./styles.css?v=0.3.6','./styles-v03.css?v=0.3.6',
+  './manifest.webmanifest?v=0.3.6','./assets/icon.svg?v=0.3.6','./js/app.js?v=0.3.6','./js/data.js?v=0.3.6',
+  './js/engine-v03.js?v=0.3.6','./js/engine-v031.js?v=0.3.6','./js/engine-v033.js?v=0.3.6','./js/engine-v036.js?v=0.3.6','./js/characters.js?v=0.3.6',
+  './js/v032-ui.js?v=0.3.6','./js/v032-formation.js?v=0.3.6','./js/v032-combat-ui.js?v=0.3.6','./js/v033-state.js?v=0.3.6','./js/v034-ui.js?v=0.3.6'
 ];
 self.addEventListener('install',(event)=>{event.waitUntil(caches.open(CACHE).then((cache)=>cache.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',(event)=>{event.waitUntil(caches.keys().then((keys)=>Promise.all(keys.filter((key)=>key.startsWith('merge-arcana-')&&key!==CACHE).map((key)=>caches.delete(key)))).then(()=>self.clients.claim()));});
